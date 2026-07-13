9817ad5568354899a3a8534daf6483bbb0543cef
akashnoyal <akashnoyal.dev@gmail.com>
2026-07-13 23:38:44 +0530

    Updated bulk mail app

--- backend/routes/mailRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const { sendMail } = require("../controllers/mailController");
-
-const router = express.Router();
-
-router.post("/send-mail", sendMail);
-
-module.exports = router;
